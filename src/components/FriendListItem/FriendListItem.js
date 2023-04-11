51bbbdfcc4b10f0722a5c7e429f2722a23529c5a
Myroslava Lukash <miralukash@gmail.com>
2023-04-11 17:29:25 +0300

    sum

--- src/components/FriendListItem/FriendListItem.js
+++ src/components/FriendListItem/FriendListItem.js
@@ -1,6 +1,9 @@
-import css from '../FriendList/FriendList.module.css'
+import css from '../FriendList/FriendList.module.css';
+import PropTypes from 'prop-types';
+
 const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (<li className={css.item}>
+  return (
+    <li className={css.item}>
           <span
             className={`${css.status} ${isOnline ? css.online : css.offline}`}
           ></span>
@@ -13,5 +16,11 @@ const FriendListItem = ({ avatar, name, isOnline }) => {
           <p className={css.name}>{name}</p>
         </li>)
 }
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
 
 export default FriendListItem;
